72bf2398afca2b3e1c7f23e9f5704c2d56cdc0da
kavously <78664463+kavously@users.noreply.github.com>
2022-05-05 01:19:43 +0200

    Add files via upload

---
+++ commande id/id.lua
@@ -0,0 +1,15 @@
+
+-- bc1q2g8xetx03edchze96xza4h30u9d6 : its my btc adress :)
+
+local function sendNotification(message)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentString(message)
+    DrawNotification(true, false)
+end
+
+RegisterCommand("id", function (_src , rawCommand)
+    local source = _src
+    local bc1q2g8xetx03edchze96xza4h30u9d6 = GetPlayerServerId(PlayerPedId())
+    
+    sendNotification("Votre Id : ~b~"..bc1q2g8xetx03edchze96xza4h30u9d6.. " ")
+end, false)
